cc9c755c316dba6906a0864c80dfc20da50ae26a
jack.edney <jack.edney@outlook.com>
2024-12-24 10:49:14 +0000

    Fixed __init__

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from pivotal_wildlife.models.detection.ultralytics_based.megadetectorv5 import (
-    MegaDetectorV5,
-)
-from pivotal_wildlife.models.detection.ultralytics_based.megadetectorv6 import (
-    MegaDetectorV6,
-)
-
-__all__ = ["MegaDetectorV5", "MegaDetectorV6"]
---
+++ pivotal_wildlife/__init__.py
@@ -0,0 +1,8 @@
+from models.detection.ultralytics_based.megadetectorv5 import (
+    MegaDetectorV5,
+)
+from models.detection.ultralytics_based.megadetectorv6 import (
+    MegaDetectorV6,
+)
+
+__all__ = ["MegaDetectorV5", "MegaDetectorV6"]
